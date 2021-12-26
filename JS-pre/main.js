a1f4ec274bdcda6eb3f2fc2e94b17f6884eb69e9
funkyblues <dnr8874@naver.com>
2021-12-26 19:40:25 +0900

    HTML/CSS/JS로 만드는 스타벅스 웹사이트 - 데이터 종류

---
+++ JS-pre/main.js
@@ -0,0 +1,16 @@
+// let myName = "changuk";
+// let email = "dnr8874@naver.com"
+// let hello = `Hello ${myName}?!`
+
+// console.log(myName);
+// console.log(email);
+// console.log(hello);
+
+let user = {
+  name: "changuk",
+  age: 26
+};
+
+console.log(user);
+console.log(user.age);
+console.log(user.name);
