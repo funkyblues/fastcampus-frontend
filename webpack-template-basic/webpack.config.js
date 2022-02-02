ecaf1477f21bea8ab1c14d1f3ff295b25de5ae89
funkyblues <dnr8874@naver.com>
2022-02-02 21:48:42 +0900

    webpack entry,output

---
+++ webpack-template-basic/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+module.exports = {
+  entry: "./js/main.js",
+  output: {
+    // path: path.resolve(__dirname, "public"),
+    // filename: "app.js",
+    clean: true,
+  },
+};
