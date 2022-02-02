add4411eeb4f7ac81808b1d628668099f8d4173f
funkyblues <dnr8874@naver.com>
2022-02-02 22:45:57 +0900

    webpack 정적 파일 연결

--- webpack-template-basic/webpack.config.js
+++ webpack-template-basic/webpack.config.js
@@ -1,9 +1,29 @@
 const path = require("path");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   entry: "./js/main.js",
   output: {
     // path: path.resolve(__dirname, "public"),
     // filename: "app.js",
     clean: true,
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: "./index.html",
+      favicon: "./static/images/logo.jpg",
+    }),
+    new CopyPlugin({
+      patterns: [
+        // 우리가 만든 폴더 static
+        // 어디에서 부터 복사해서 dist라는 폴더로 복사할 것인지에 대해 명시
+        { from: "static" },
+      ],
+    }),
+  ],
+
+  devServer: {
+    host: "localhost",
   },
 };
